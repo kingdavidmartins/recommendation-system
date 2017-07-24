c7e070798bd3c1f2362f9216452e468562970b56
King <kingdavidmartins@users.noreply.github.com>
2017-07-24 13:40:05 -0400

    update manhattanDistance with N dimensions, also update README

--- formula/manhattanDist/README.md
+++ formula/manhattanDist/README.md
@@ -5,6 +5,7 @@ The Manhattan Distance
   1. Learn basic formula / concept ~ Manhattan Distance
   2. Create a function that computes the Manhattan distance of a 2-Dimensional set
   4. Update the function so that it computes the Manhattan distance on a N-Dimensional set (vector with N length) ~ assuming all points on the vector pairs are the same length, with no empty values/points
+  5. Round manhattan distance to the nearest thousandth place value
 
 
 ![Alt Text](http://angiogenesis.dkfz.de/oncoexpress/software/cs_clust/dist_004.gif)
--- formula/manhattanDist/manhattanDist.js
+++ formula/manhattanDist/manhattanDist.js
@@ -1,16 +1,38 @@
+const _ = require('lodash');
+
 // case example 1 [2 Dimensional data set]
 const hailey = [4, 4];
 const veronica = [5, 3];
 const jim = [4, 72];
 const joe = [23, 2];
 
+// case example 2 [N Dimensional data set]
+const h = [4, 1, 4, 4, 1];
+const j = [4.5, 4, 5, 4, 4];
+const a = [3, 43, 2, 5, 7, 9, 23.7, 27, 9, 7, 1, 6, 9, 53.4];
+const b = [63.7, 3, 6, 112.45, 8, 21.5, 7, 5, 1.89, 4, 6, 8, 5, 8];
+const c = [3, 5, 7, 89, 4, 8, 5, 9];
+const d = [3, 5, 7, 88.99, 4, 8, 6, 9];
+
+// N Dimensions
 const manhattanDist = (user1, user2) =>
-  Math.abs(user1[0] - user2[0])
-  + Math.abs(user1[1] - user2[1])
+  _.chain(user1)
+    .map((value, index) =>
+      Math.abs(user1[index] - user2[index]))
+    .reduce((a, b) => a + b)
+    .value()
+    .toFixed(3);
 
 console.log(
-  manhattanDist(hailey, veronica), // => 2
-  manhattanDist(hailey, jim), // => 68
-  manhattanDist(joe, veronica), // => 19
-  manhattanDist(jim, joe) // => 89
+
+  // 2 D pair
+  manhattanDist(hailey, veronica), // => 2.000
+  manhattanDist(hailey, jim), // => 68.000
+  manhattanDist(joe, veronica), // => 19.000
+  manhattanDist(jim, joe), // => 89.000
+
+  // N D pair
+  manhattanDist(h, j), // => 7.500
+  manhattanDist(a, b), // => 330.860
+  manhattanDist(c, d) // => 1.010
   );
