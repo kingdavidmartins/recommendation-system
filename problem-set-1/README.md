8c1df8bbbd47643a2365bedc979bdd739097b720
King <kingdavidmartins@users.noreply.github.com>
2017-07-26 19:44:05 -0400

    updated README.md & solution.js

--- problem-set-1/README.md
+++ problem-set-1/README.md
@@ -1,5 +1,21 @@
 # Problem Set 1 - Recommendation Engine's
-## Project to Implement different data mining algorithm & techniques
+
+## Intro
+Finally, we are going to put this all the formula's together to make recommendations. Given a collection of users that rated bands. Let's say I want to
+make recommendations for userX. I want apply the appropriate formula to find the nearest neighbor—userZ in this case. I will
+then find bands that userZ has rated but userX has not. Also, I will assume that userX
+would have rated the bands the same as (or at least very similar to) userZ. For example,
+userX has not rated the great band Phoenix. userZ has rated Phoenix a '4' so we will
+assume userX is likely to enjoy the band as well. You will be tasked with solving these type problems, which will teach you how to make recommendations.
+
+
+Problem
+---------
+
+Solution
+---------
+
+
 
 # References
 [Guide to Data Mining](http://guidetodatamining.com)
--- problem-set-1/solution.js
+++ problem-set-1/solution.js
@@ -1,6 +1,66 @@
+const _ = require('lodash');
 const users = require('./users');
 
-// Basic Hello world for all the users lol
+// function to compute the minkowski distance
+const minkowskiDist = (user1, user2, p) =>
+  Math
+    .pow(
+      _.chain(user1)
+        .map((value, index) =>
+          Math.pow(Math.abs(user1[index] - user2[index]), p))
+        .reduce((a, b) => a + b)
+        .value(), 1/p)
+    .toFixed(3);
+
+// function that maps the user's rating number
+const userRating = (usrName) =>
+  usrName
+    .map((obj) => obj.number);
+
+// syncRatings is a function that take 2 arguments: (userConst, user2)
+// userConst: is the consistent user that will be compared with all other users in the set
+// user2: is all the different users that will be compared against the consistent user
+// syncRatings is a function that compares all ratings of the potential users with N Dimensions
+// by filtering the ratings that does not match the comparator
+// i.e if { user: john, ratings: [ {artist: a, number: 4}, {artist: b, number: 3} ]}
+// and { user: doe, ratings: [ {artist: a, number: 1}]}
+// I will only wnat to check the minkowski distance of what they both rated
+// which should end up computing the minkowski distance their 2 ratings: [ {artist: a, number: 1}]
+const syncRatings = (userConst, user2) =>
+  [
+    _.find(users, { name: userConst})
+      .ratings
+      .filter((value) =>
+        _.find(users, { name: user2})
+          .ratings
+          .filter((obj) =>
+            _.find(users, { name: userConst})
+              .ratings
+              .map((cObj) => cObj.artist)
+              .indexOf(obj.artist) !== -1)
+          .map((mObj) => mObj.artist)
+          .indexOf(value.artist) !== -1),
+    _.find(users, { name: user2})
+      .ratings
+      .filter((obj) =>
+        _.find(users, { name: userConst})
+          .ratings
+          .map((cObj) => cObj.artist)
+          .indexOf(obj.artist) !== -1)
+  ];
+
+const simUserSort = (mainUser, userSet, p) =>
+  _.chain(userSet)
+    .filter((obj) => obj.name !== mainUser)
+    .map((obj) =>
+      minkowskiDist(
+        userRating(syncRatings('Hailey', obj.name)[0]),
+        userRating(syncRatings('Hailey', obj.name)[1]),
+        1))
+    .value()
+    .sort((a, b) => a - b);
+
+// logs minkowski distance of all the users in order from least to greatest
 console.log(
-  users.map(x => `Hello ${x.name}`)
+  simUserSort('Hailey', users, 1)
 );
