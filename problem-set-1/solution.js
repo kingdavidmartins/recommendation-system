eee7748bada5eeaf56f2cc531bffbb6aaf80cafd
King <kingdavidmartins@users.noreply.github.com>
2017-08-01 01:25:51 -0400

    add comments to simUserSort fn & fix simUserSort so I pass Main user in function properly

--- problem-set-1/solution.js
+++ problem-set-1/solution.js
@@ -49,18 +49,50 @@ const syncRatings = (userConst, user2) =>
           .indexOf(obj.artist) !== -1)
   ];
 
+// simUserSort is a function that takes 3 arguments. the 1st is mainUser
+// which is user you want to compare to the different set of users. The 2nd is
+// the userSet. The 3rd is p which stands for the correlation coefficient which will be passed
+// through when using the minkowskiDist() function. It will return an 2 Dimensional with each element represendting an array with 2 elements.
+// the 1st element standing for the name of the user from the set.
+// the 2nd element standing for the distance from the mainUser from the user ~ (which is represented by the name in the 1st eleent)
+// the every element is then sorted from least to greatest which make the 1st element the nearest neighbor
 const simUserSort = (mainUser, userSet, p) =>
   _.chain(userSet)
     .filter((obj) => obj.name !== mainUser)
     .map((obj) =>
-      minkowskiDist(
-        userRating(syncRatings('Hailey', obj.name)[0]),
-        userRating(syncRatings('Hailey', obj.name)[1]),
-        1))
+      [
+        obj.name,
+        minkowskiDist(
+          userRating(syncRatings(mainUser, obj.name)[0]),
+          userRating(syncRatings(mainUser, obj.name)[1]),
+          p)
+      ]
+    )
     .value()
-    .sort((a, b) => a - b);
+    .sort((a, b) => a[1] - b[1]);
 
-// logs minkowski distance of all the users in order from least to greatest
+// TEST 1
+/*
+  manhattan(users['Hailey'], users['Veronica']) ==> 2.0
+  manhattan(users['Hailey'], users['Jordyn']) == > 7.5
+>>> */
+console.log(
+
+);
+console.log(
+  'Manhattan Distance'
+);
 console.log(
   simUserSort('Hailey', users, 1)
+);
+
+console.log(
+  '\n\n'
+);
+
+console.log(
+  'Euclidean Distance'
+);
+console.log(
+  simUserSort('Hailey', users, 2)
 );
