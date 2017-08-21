d8782d951957b9968c875551c80a5fb6e872f119
King <kingdavidmartins@users.noreply.github.com>
2017-08-21 05:50:41 -0400

    update tranform.js to process and add all json -> txt files

--- recommendation-engine-1/transform.js
+++ recommendation-engine-1/transform.js
@@ -4,9 +4,14 @@ const _ = require('lodash');
 const objify = (obj) => JSON.parse(obj);
 const structNewObj = (value) => JSON.parse(JSON.stringify(value));
 
+
 // reads movieLenRaw100k.js process and writes to movieLenTransform100k.txt
-fs.readFile('movieLenRaw100k.js', 'utf8' , (err, data) => {
-  let newData = _.chain(Array(671).fill())
+fs.readFile('movieLenRaw100k.js', 'utf8', (err, data) => {
+
+  // grabs the last userId which represents the total amount of users the create array of user
+  let totalUser = objify(data)[objify(data).length - 1].userId;
+
+  let data100k = _.chain(Array(totalUser).fill())
     .map((value, index) =>
       structNewObj({
         name: index + 1,
@@ -14,10 +19,10 @@ fs.readFile('movieLenRaw100k.js', 'utf8' , (err, data) => {
       }))
     .value();
 
-    // process and push to newData
+    // process and push to data100k
   _.chain(objify(data))
     .forEach((obj, index) =>
-      _.find(newData, {name: obj.userId})
+      _.find(data100k, {name: obj.userId})
         .ratings
         .push(
           structNewObj({
@@ -26,7 +31,73 @@ fs.readFile('movieLenRaw100k.js', 'utf8' , (err, data) => {
           })))
     .value()
 
-  // write newData to movieLenTransform100k.txt
-  fs.writeFile('movieLenTransform100k.txt', JSON.stringify(newData), (err) =>
+  // write data100k to movieLenTransform100k.txt
+  fs.writeFile('movieLenTransform100k.txt', JSON.stringify(data100k), (err) =>
     console.log(err ? `Err: ${err}` : `movieLenTransform100k was saved`));
+});
+
+
+
+// reads movieLenRaw20M.js process and writes to movieLenTransform20M.txt
+fs.readFile('movieLenRaw20M.js', 'utf8', (err, data) => {
+
+  // grabs the last userId which represents the total amount of users the create array of user
+  let totalUser = objify(data)[objify(data).length - 1].userId;
+
+  let data20M = _.chain(Array(totalUser).fill())
+    .map((value, index) =>
+      structNewObj({
+        name: index + 1,
+        ratings: []
+      }))
+    .value();
+
+    // process and push to data20M
+  _.chain(objify(data))
+    .forEach((obj, index) =>
+      _.find(data20M, {name: obj.userId})
+        .ratings
+        .push(
+          structNewObj({
+            movie: obj.movieId,
+            number: obj.rating
+          })))
+    .value()
+
+  // write data20M to movieLenTransform20M.txt
+  fs.writeFile('movieLenTransform20M.txt', JSON.stringify(data20M), (err) =>
+    console.log(err ? `Err: ${err}` : `movieLenTransform20M was saved`));
+});
+
+
+
+// reads movieLenRaw26M.js process and writes to movieLenTransform26M.txt
+fs.readFile('movieLenRaw26M.js', 'utf8' , (err, data) => {
+
+  // grabs the last userId which represents the total amount of users the create array of user
+  let totalUser = objify(data)[objify(data).length - 1].userId;
+
+  let data26M = _.chain(Array(totalUser).fill())
+    .map((value, index) =>
+      structNewObj({
+        name: index + 1,
+        ratings: []
+      }))
+    .value();
+
+    // process and push to data26M
+  _.chain(objify(data))
+    .forEach((obj, index) =>
+      _.find(data26M, {name: obj.userId})
+        .ratings
+        .push(
+          structNewObj({
+            movie: obj.movieId,
+            number: obj.rating
+          })))
+    .value()
+
+  // write data26M to movieLenTransform26M.txt
+  fs.writeFile('movieLenTransform26M.txt', JSON.stringify(data26M), (err) =>
+    console.log(err ? `Err: ${err}` : `movieLenTransform26M was saved`));
 });
