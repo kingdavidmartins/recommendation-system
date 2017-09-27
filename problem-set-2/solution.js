9914f79811ca2e11f7052f5f76c11c702f991e0b
King <kingdavidmartins@users.noreply.github.com>
2017-09-27 17:24:10 -0400

    update problem-set-2 | in progress

--- problem-set-2/solution.js
+++ problem-set-2/solution.js
@@ -1,1 +1,40 @@
 const _ = require('lodash');
+const users = require('./users');
+
+const adjustedCosineSim = (userSet) => {
+  let average = userSet
+                  .map(usr =>
+                    usr
+                      .ratings
+                      .map(rating => rating.number)
+                      .reduce((a, b) => a + b))
+                  .map((sum, index) => sum/userSet[index].ratings.length);
+
+  let formatUsrSet = userSet
+                      .map((user, index) =>
+                        JSON.parse(
+                          JSON.stringify({
+                            name: user.name,
+                            averRating: average[index],
+                            ratings: user.ratings
+                          })
+                        ));
+
+  let allUniqueNames = [];
+
+  formatUsrSet
+    .forEach(user =>
+      user
+        .ratings
+        .forEach(rating =>
+          (allUniqueNames.indexOf(rating.artist) === -1)
+            ? allUniqueNames.push(rating.artist)
+            : null ));
+
+
+
+  return allUniqueNames;
+}
+
+
+console.log(adjustedCosineSim(users));
