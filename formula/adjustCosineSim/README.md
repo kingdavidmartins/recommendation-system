624c122e5dea02bcd83afdfdcaf808f0a1a5c675
King <kingdavidmartins@users.noreply.github.com>
2017-09-26 20:14:41 -0400

    init adjusted cosine sim & and practice usr set for problem set 2

---
+++ formula/adjustCosineSim/README.md
@@ -0,0 +1,10 @@
+Adjusted Cosine Similarity
+==========================
+
+**Roadmap**
+  1. Learn basic formula / concept ~  Adjusted Cosine Similarity
+  2. Create a function that computes the adjusted cosine similarity of a N-Dimensional set (vector with N length)
+  3. Round the adjusted cosine similarity to the nearest thousandth place value
+
+
+![Alt Text](https://i.stack.imgur.com/Oo6dl.png)
---
+++ problem-set-2/users.js
@@ -0,0 +1,111 @@
+module.exports = [
+  {
+    name: 'David',
+    ratings: [
+      {
+        artist: 'Imagine Dragons',
+        number: 3
+      },
+      {
+        artist: 'Daft Punk',
+        number: 5
+      },
+      {
+        artist: 'Lorde',
+        number: 4
+      },
+      {
+        artist: 'Fall Out Boy',
+        number: 1
+      }
+    ]
+  },
+  {
+    name: 'Matt',
+    ratings: [
+      {
+        artist: 'Imagine Dragons',
+        number: 3
+      },
+      {
+        artist: 'Daft Punk',
+        number: 4
+      },
+      {
+        artist: 'Lorde',
+        number: 4
+      },
+      {
+        artist: 'Fall Out Boy',
+        number: 1
+      }
+    ]
+  },
+  {
+    name: 'Ben',
+    ratings: [
+      {
+        artist: 'Imagine Dragons',
+        number: 3
+      },
+      {
+        artist: 'Kacey Musgraves',
+        number: 4
+      },
+      {
+        artist: 'Lorde',
+        number: 3
+      },
+      {
+        artist: 'Fall Out Boy',
+        number: 1
+      }
+    ]
+  },
+  {
+    name: 'Chris',
+    ratings: [
+      {
+        artist: 'Imagine Dragons',
+        number: 4
+      },
+      {
+        artist: 'Daft Punk',
+        number: 4
+      },
+      {
+        artist: 'Lorde',
+        number: 3
+      },
+      {
+        artist: 'Fall Out Boy',
+        number: 1
+      },
+      {
+        artist: 'Kacey Musgraves',
+        number: 4
+      }
+    ]
+  },
+  {
+    name: 'Tori',
+    ratings: [
+      {
+        artist: 'Imagine Dragons',
+        number: 4
+      },
+      {
+        artist: 'Daft Punk',
+        number: 5
+      },
+      {
+        artist: 'Kacey Musgraves',
+        number: 5
+      },
+      {
+        artist: 'Fall Out Boy',
+        number: 3
+      }
+    ]
+  }
+];
